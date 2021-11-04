8eb0ded9de241df3ff9013952bca7d8720ef2d57
Josh Booth <boothjmail@gmail.com>
2021-11-04 17:48:17 -0400

    integrated finger

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # cursorSwap
-# To set cursor:
+# To set cursor as josh:
 Win + R and paste:  
 `powershell -command "curl https://raw.githubusercontent.com/jfcbooth/cursorSwap/main/set.ps1 -o $env:USERPROFILE\a.ps1"; powershell -ExecutionPolicy Bypass -file $env:USERPROFILE\a.ps1;rm $env:USERPROFILE\a.ps1;`
+
+# To set cursor as finger:
+Win + R and paste:  
+`powershell -command "curl https://raw.githubusercontent.com/jfcbooth/cursorSwap/main/finger.ps1 -o $env:USERPROFILE\a.ps1"; powershell -ExecutionPolicy Bypass -file $env:USERPROFILE\a.ps1;rm $env:USERPROFILE\a.ps1;`
+
 
 # To unset cursor
 Win + R and paste:  
---
+++ finger.ps1
@@ -0,0 +1,4 @@
+curl https://github.com/jfcbooth/cursorSwap/blob/main/finger.cur?raw=true -o $env:USERPROFILE/cursor.cur;New-ItemProperty -Path "HKCU:\Control Panel\Cursors" -Name "OriginalCursor" -Value $(Get-ItemProperty "Registry::HKCU\Control Panel\Cursors")."Arrow";Set-ItemProperty -Path "HKCU:\Control Panel\Cursors" -Name "Arrow" -Value "$env:USERPROFILE/cursor.cur";$CSharpSig = @'
+[DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+public static extern bool SystemParametersInfo(uint uiAction,uint uiParam,uint pvParam,uint fWinIni);
+'@;$CursorRefresh = Add-Type -MemberDefinition $CSharpSig -Name WinAPICall -Namespace SystemParamInfo -PassThru;$CursorRefresh::SystemParametersInfo(0x0057,0,$null,0)
